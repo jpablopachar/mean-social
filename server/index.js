9cc7277629f124399ecfe165a772ab506faf3814
jpablopachar <jpablopachar1993@gmail.com>
2018-05-12 00:03:36 -0500

    We configure the http headers

--- server/index.js
+++ server/index.js
@@ -26,6 +26,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Converts the data arriving through HTTP requests to JSON type
 app.use(bodyParser.json());
 
+/*           Configure HTTP headers           */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  next();
+});
+
 /*                   Routes                      */
 app.use('/api', userRoutes);
 app.use('/api', followRoutes);
